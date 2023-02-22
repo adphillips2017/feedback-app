8a0d8e04105d9252110bd4666b287449247dbda5
Adam Phillips <adphillips2017@users.noreply.github.com>
2023-02-22 15:55:55 -0500

    adding ability to delete feedback items

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import FeedbackData from './data/FeedbackData'
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
+  const deleteFeedbackById = (id) => {
+    if (!window.confirm('Are you sure you want to delete?')) return
+
+    setFeedback(feedback.filter(item => item.id !== id))
+  }
+
   return (
   <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList
+          feedback={feedback}
+          handleDelete={deleteFeedbackById}
+        />
       </div>
   </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,24 @@
+import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-function FeedbackItem({rating, text}) {
+function FeedbackItem({rating, text, id, handleDelete}) {
   return (
     <Card dark={ false }>
         <div className="num-display">{rating}</div>
+        <button onClick={() => handleDelete(id)} className="close">
+          <FaTimes color="purple"/>
+        </button>
         <div className="text-display">{text}</div>
     </Card>
   )
 }
 
 FeedbackItem.propTypes = {
-  item: PropTypes.object.isRequired
+  id: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  handleDelete: PropTypes.func.isRequired
 }
 
 export default FeedbackItem
