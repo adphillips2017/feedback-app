a8a740fb3f0f8798fd00c6f3268022b5e353a5b7
Adam Phillips <adphillips2017@users.noreply.github.com>
2023-03-02 08:45:31 -0500

    setting up jsonServer backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,15 +1,34 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [useBackend, setUseBackend] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  useEffect(() => {
+    if (!useBackend) {
+      setFeedback(FeedbackData)
+      setIsLoading(false)
+    } else {
+      fetchFeedback()
+    }
+  }, [])
+
+  const fetchFeedback = async () => {
+    const response = await fetch('http://localhost:5000/feedback?_sort=id&_oder=desc')
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const deleteFeedbackById = (id) => {
     if (!window.confirm('Are you sure you want to delete?')) return
@@ -39,6 +58,7 @@ export const FeedbackProvider = ({children}) => {
     <FeedbackContext.Provider value={{
       feedback,
       feedbackEdit,
+      isLoading,
       deleteFeedbackById,
       addFeedback,
       updateFeedback,
